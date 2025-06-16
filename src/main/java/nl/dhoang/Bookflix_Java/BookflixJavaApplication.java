0f04203c7b064fd01de46e966c05c2492e8597cb
duyhoang <duyhoang123@hotmail.co.uk>
2025-06-16 16:01:45 +0100

    initial commit

---
+++ src/main/java/nl/dhoang/Bookflix_Java/BookflixJavaApplication.java
@@ -0,0 +1,13 @@
+package nl.dhoang.Bookflix_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookflixJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookflixJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/dhoang/Bookflix_Java/BookflixJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.dhoang.Bookflix_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookflixJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
